3953a0468dbb360489e41d0949960c67624189d8
Angel Velasquez <pcisavel@upc.edu.pe>
2023-08-31 18:08:29 -0500

    feat(main): implemented reaction on developer registered.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,30 @@
-<script setup>
+<script>
 import Welcome from "./welcoming/components/welcome.component.vue";
 import RegisterDeveloper from "./welcoming/components/register-developer.component.vue";
+export default {
+  name: "app",
+  components: {Welcome, RegisterDeveloper},
+  data() {
+    return {
+      firstName: '',
+      lastName: ''
+    }
+    },
+  methods: {
+    onDeveloperRegistered(developer) {
+      this.firstName = developer.firstName;
+      this.lastName = developer.lastName;
+      console.log("Developer registered: ", developer);
+    }
+  }
+
+}
+
 </script>
 
 <template>
   <h1>Hello Vue Developer Application</h1>
-  <register-developer />
-  <welcome first-name="John" last-name="Doe" />
+  <register-developer v-on:developer-registered="onDeveloperRegistered" />
+  <welcome :first-name="firstName" :last-name="lastName" />
 </template>
 
