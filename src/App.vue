fa6aacbe91f17189e1f52e7449b07d6420662fa1
Angel Velasquez <pcisavel@upc.edu.pe>
2023-08-31 17:58:49 -0500

    feat(register-developer): added register developer component.

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <script setup>
 import Welcome from "./welcoming/components/welcome.component.vue";
+import RegisterDeveloper from "./welcoming/components/register-developer.component.vue";
 </script>
 
 <template>
+  <h1>Hello Vue Developer Application</h1>
+  <register-developer />
   <welcome first-name="John" last-name="Doe" />
 </template>
 
