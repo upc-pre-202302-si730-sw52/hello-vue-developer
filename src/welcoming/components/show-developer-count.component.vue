611af1533d1616bda203664b76e01ed41c158d51
Angel Velasquez <pcisavel@upc.edu.pe>
2023-08-31 18:21:49 -0500

    feat(show-developer-count): added show developer count component.

---
+++ src/welcoming/components/show-developer-count.component.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  name: "show-developer-count",
+  data() {
+    return {
+      developerCount: 0
+    };
+  },
+  methods: {
+    onDeveloperRegistered(developer) {
+      if (developer.fullName !== "Unknown")
+        this.developerCount++;
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <p>Developers registered: {{ developerCount }}</p>
+    <p>Note: Unknown developers are not considered.</p>
+  </div>
+</template>
