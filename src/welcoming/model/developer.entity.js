6a597dcb5e4f23e6c887334378d14d115bf64a18
Angel Velasquez <pcisavel@upc.edu.pe>
2023-08-31 17:32:05 -0500

    chore(developer): added developer entity.

---
+++ src/welcoming/model/developer.entity.js
@@ -0,0 +1,14 @@
+class Developer {
+    firstName;
+    lastName;
+
+    constructor(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    get fullName() {
+        return this.firstName == null || this.lastName == null ? "Unknown" : `${this.firstName} ${this.lastName}`.trim();
+    }
+
+}
